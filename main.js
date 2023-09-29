08eb02e5475cb7f36b4ae31c9589854c52f76b2f
“Kevin-Jimenez-D” <122753141+Kevin-Jimenez-D@users.noreply.github.com>
2023-09-29 08:41:56 -0500

    feat: :sparkles: Encontré una mejor forma de visualizar los datos, y de ahí tener una mejor lógica para los métodos
    Con esa lógica aplicaré todos los métodos para que se pueda jugar con la base de datos

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const traerMockApi = async()=>{
+/*const traerMockApi = async()=>{
     let mockapi = await (await fetch("https://6509e7e7f6553137159c3ae5.mockapi.io/presupuestoCasa")).json();
     //console.log(mockapi);
     let config=
@@ -13,3 +13,44 @@ const traerMockApi = async()=>{
 }
 
 traerMockApi();
+
+
+const actualizar = async(Id)=>{
+    let config=
+    {
+        method:"PUT",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify({
+            caja:"ingreso",
+            valor: 0,
+            id : Id
+        })
+    }
+    let res = await (await fetch(`http://localhost:3000/libros`, config)).json();
+    console.log(res);
+}
+actualizar(23);
+
+
+const eliminar = async(id)=>{
+    let config=
+    {
+        method:"DELETE",
+    }
+    let res = await (await fetch(`http://localhost:3000/libros/${id}`, config)).json();
+    console.log(res);
+}
+
+eliminar(3);
+
+*/
+
+const mostrarLibros=async()=>{
+    let res = await (await fetch(`http://localhost:3000/libros`)).json();
+    console.log(res[0][0]["id"])    //Segun esta lógica, el primer cero me muestra todos los datos (debe ser fijo)
+                                    //El segundo cero me muestra de todos esos datos la posicion "del vector", inicia desde cero
+                                    //y el tercer dato accedo al objeto que deseo
+                                    //Con esta lógica usar todos los métodos
+}
+
+mostrarLibros()
