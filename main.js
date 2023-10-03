c3feb568596a6526e2e2f6ce40315acf5b5c8829
Kevin-Jimenez-D <122753141+Kevin-Jimenez-D@users.noreply.github.com>
2023-10-03 07:02:51 -0500

    feat: :construction: Envia los datos pero con la id que no es, necesito resolver ese detalle
    Cuando agrego nuevos datos en la tabla, este me los agrega pero me coloca unas ids diferentes, debo corregir esto

--- main.js
+++ main.js
@@ -74,7 +74,7 @@ addEventListener("DOMContentLoaded", async()=>{
     let res = await (await fetch("https://6509e7e7f6553137159c3ae5.mockapi.io/presupuestoCasa")).json();
     //console.log(res);            //Aca guarda en un array lo que trae del HTML, como en forma de diccionario
     //extrae cada uno de los elementos con el for y los va agregando al HTML
-    console.log(res);
+    //console.log(res);
     for(let i=0; i< res.length ; i++){
         myTabla.insertAdjacentHTML("beforeend", `
         <tr>
@@ -188,3 +188,120 @@ enviarJsonServer.addEventListener("submit", async(e)=>{
     */
 })
 
+
+/*
+let myfrom=document.querySelector("form");       //Va a apuntar a todos los forms
+//Agregar los valores de la tabla en el html y en el JSON Server
+//stringify para pasarlo por una web en forma de cadena
+//en el id del mockapi el presupuestoCasa es el nombre de la API que aparece enla aplicacion al abrirla
+myfrom.addEventListener("submit", async(e)=>{
+    e.preventDefault();
+    const data=Object.fromEntries(new FormData(e.target));
+    console.log(data);
+    const {valor} =data;
+    data.valor=(typeof valor === "string") ? Number(valor) : null;
+    let config={
+        method:"POST",
+        headers : {"content-type" : "application/json"},
+        body: JSON.stringify(data)
+    };
+    let res = await (await fetch(`http://localhost:3000/libros`,config)).json();
+    //console.log(res);
+})
+*/
+
+
+
+
+
+
+//BUSCARV
+let searchForm = document.querySelector("#searchForm"); // selecciona el formulario de búsqueda donde se evidenciará los resultados al colocar el ID
+let searchTableBody = document.querySelector("#search"); // selecciona el tbody de búsqueda donde se evidenciara en el HTML la ID buscada
+//BUSCARV
+
+//BUSCARV Hace la busqueda al colocar la ID, y darle en buscar ID, en caso de que no exista, dice que no existe y se ve en el HTML
+//searchForm con el id #searchForm es la tabla donde se buscaran las IDs, si se le da submit, buscara el evento en ese formulario
+searchForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    //En este caso, está buscando un elemento <input> que tenga un atributo name igual a "ID", que esta en el HTML de la tabla para buscar
+    //Ese dato, que es el que ingresa el usuario, es el que comparara con el array
+    const idToSearch = document.querySelector("input[name='ID']").value;
+    
+    // Realiza la búsqueda de la ID en los datos (busca en el array datosID si esta la ID que coloco el usuario)
+    const foundIndex = datosID.indexOf(idToSearch);
+    
+    if (foundIndex !== -1) {
+        // La ID fue encontrada, muestra el resultado en la tabla de búsqueda
+        //console.log(foundIndex);   
+        //console.log(datosValor);
+        //console.log(datosCaja);
+        //Se evidencia en el HTML
+        searchTableBody.innerHTML = `
+            <tr>
+                <td>${datosID[foundIndex]}</td>
+                <td>${datosValor[foundIndex]}</td>
+                <td>${datosCaja[foundIndex]}</td>
+            </tr>
+        `;
+
+    } else {
+        // La ID no fue encontrada, muestra un mensaje de error o haz lo que necesites
+        searchTableBody.innerHTML = `<tr><td colspan="3">ID no encontrada</td></tr>`;
+    }
+});
+
+//BUSCARV
+
+
+
+
+
+//ACTUALIZAR
+const actualizarForm=document.querySelector("#actualizar");  //Para rastrear el evento cuando se vaya a actualizar algo, como si se actualizara toda la pagina
+//ACTUALIZAR
+
+//ACTUALIZAR
+actualizarForm.addEventListener("submit", async (e) => {
+    //Saldo total 
+    
+        //lo hace bien pero solo cuando oprimo el boton Buscar ID
+        // Iterar a través de los datos y clasificarlos en los arrays correspondientes
+        for (let i = 0; i < datosCaja.length; i++) {
+            if (datosCaja[i] === 'ingreso') {
+                valoresIngreso.push(datosValor[i]);
+            } else if (datosCaja[i] === 'egreso') {
+                valoresEgreso.push(datosValor[i]);
+            }
+        }
+
+        // Sumar todos los valores de ingreso
+        let sumaValoresIngreso = valoresIngreso.reduce((total, valor) => total + valor, 0);
+
+        // Sumar todos los valores de egreso
+        let sumaValoresEgreso = valoresEgreso.reduce((total, valor) => total + valor, 0);
+
+        // Ahora, tienes dos arrays: valoresIngreso y valoresEgreso, que contienen los valores según su tipo de caja
+        //console.log('Valores de Ingreso:', valoresIngreso);
+        //console.log('Valores de Egreso:', valoresEgreso);
+
+        // Imprimir las sumas
+        //console.log('Suma de Valores de Ingreso:', sumaValoresIngreso);
+        //console.log('Suma de Valores de Egreso:', sumaValoresEgreso);
+
+    //Saldo total
+
+
+    //Saldo total
+    showTotal.innerHTML = `
+    <tr>
+        <td>${sumaValoresIngreso}</td>
+        <td>${sumaValoresEgreso}</td>
+        <td>${sumaValoresIngreso-sumaValoresEgreso}</td>
+    </tr>
+    `;
+    
+    //Saldo total
+});
+//ACTUALIZAR
